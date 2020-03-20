f289f01cbeefe1c9a768be9cbdca66eb33f50657
mswgen <54498184+mswgen@users.noreply.github.com>
2020-03-20 18:54:37 +0900

    Update template.js

--- module/template.js
+++ module/template.js
@@ -5,8 +5,20 @@ module.exports = {
         <html>
         <head>
         <link href="style.css" rel="stylesheet">
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
+        <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
+        <script>
+            var yes = false;
+            var onloadCallback = function() {
+                grecaptcha.render('html_element', {
+                    'sitekey' : '6LfdkOEUAAAAAHZQhQujPAnVqMQp177OZBO2vc3M',
+                    'callback': function(){
+                        yes = true;
+                    }
+                });
+            };
+        </script>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
         </head>
         <body>
             <h1><a href="/">WEB</a></h1>
